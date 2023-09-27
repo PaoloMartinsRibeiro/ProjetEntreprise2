1a0315b5ae6aa7181cb8228c0b24940939d311fd
PaoloMartinsRibeiro <119572269+PaoloMartinsRibeiro@users.noreply.github.com>
2023-09-27 22:32:49 +0200

    fix(css): fixing css errors

--- front/front/src/Pages/Home/Home.jsx
+++ front/front/src/Pages/Home/Home.jsx
@@ -3,13 +3,17 @@ import Salaries from "../Salaries/Salaries"
 import Services from "../Services/Services"
 import Sites from "../Sites/Sites"
 import SearchButton from "../SearchButton/SearchButton"
+import "../Home/Home.css"
 
 function Home() {
 
     return (
-        <div>
-            <h1>Annuaire d'entreprise</h1>
-            <SearchButton />
+        <div className="home-container">
+            <div className="centered-content">
+                <h1>Annuaire d'entreprise</h1>
+                <SearchButton />
+            </div>
+
             <h2>Liste des salariés</h2>
             <Salaries />
             <h2>Liste des services</h2>
